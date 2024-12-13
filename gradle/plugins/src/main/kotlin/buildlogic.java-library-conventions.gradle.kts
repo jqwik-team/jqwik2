094e94cc915b047d74449ce49f3d0deeef47daa3
Johannes Link <business@johanneslink.net>
2024-12-13 10:17:35 +0100

    Extracted publish-library build logic

--- gradle/plugins/src/main/kotlin/buildlogic.java-library-conventions.gradle.kts
+++ gradle/plugins/src/main/kotlin/buildlogic.java-library-conventions.gradle.kts
@@ -1,10 +1,7 @@
-import groovy.lang.ExpandoMetaClassCreationHandle.disable
-
 plugins {
     id("buildlogic.java-common-conventions")
+    id("buildlogic.publish-library")
     `java-library`
-    `maven-publish`
-    signing
 }
 
 java {
@@ -16,81 +13,4 @@ tasks.withType<Javadoc> {
     (options as StandardJavadocDocletOptions).addStringOption("Xdoclint:none", "-quiet")
     isFailOnError = false
     include("**/api/**")
-}
-
-val jqwik2Version: String = "${rootProject.extra.get("jqwik2Version")}"
-val isSnapshotRelease = jqwik2Version.endsWith("SNAPSHOT")
-
-
-interface PublishLibraryExtension {
-    val artifactId: Property<String>
-}
-
-val publishLibraryExtension = project.extensions.create<PublishLibraryExtension>("publishLibrary")
-publishLibraryExtension.artifactId.convention("artifactId-not-set")
-
-publishing {
-    repositories {
-        maven {
-            // hint: credentials are in ~/.gradle/gradle.properties
-            val repoUsername: String = project.findProperty("tokenUsername") as? String ?: ""
-            val repoPassword: String = project.findProperty("tokenPassword") as? String ?: ""
-
-            credentials {
-                username = repoUsername
-                password = repoPassword
-            }
-
-            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-            url = uri(
-                if (isSnapshotRelease) {
-                    snapshotsRepoUrl
-                } else {
-                    releasesRepoUrl
-                }
-            )
-        }
-    }
-    publications {
-        create<MavenPublication>("jqwikModule") {
-            groupId = "net.jqwik"
-            artifactId = "placeholder"
-            from(components["java"])
-            pom {
-                version = jqwik2Version
-                groupId = "net.jqwik"
-                name = artifactId
-                description = "$description"
-                url = "https://github.org/jqwik-team/jqwik2"
-                licenses {
-                    license {
-                        name = "Eclipse Public License - v 2.0"
-                        url = "http://www.eclipse.org/legal/epl-v20.html"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "jlink"
-                        name = "Johannes Link"
-                        email = "business@johanneslink.net"
-                    }
-                }
-                scm {
-                    connection = "scm:git:git://github.com/jqwik-team/jqwik2.git"
-                    developerConnection = "scm:git:git://github.com/jqwik-team/jqwik2.git"
-                    url = "https://github.com/jqwik-team/jqwik2"
-                }
-            }
-            afterEvaluate {
-                artifactId = publishLibraryExtension.artifactId.get()
-            }
-        }
-    }
-}
-
-signing {
-    if (!isSnapshotRelease) {
-        sign(publishing.publications["jqwikModule"])
-    }
 }
